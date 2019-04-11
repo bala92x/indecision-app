92e47d9dfe123f84f6ca07f8f138e360ba77bafb
Budavölgyi Bálint <bala92x@gmail.com>
2019-04-11 09:06:04 +0200

    Setup for heroku deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const publicPath = path.join(__dirname, 'public')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: publicPath,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]   
-            }
-        ]
-    },
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: publicPath
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    const CSSExtract = new ExtractTextPlugin('styles.css')
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(publicPath, 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    })  
+                }
+            ]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: publicPath,
+            port: 8080,
+            publicPath: '/dist/'
+        }
     }
 }
